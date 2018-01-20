d52e5209f52930137d2499926f2d809c0568d4d6
Yuchao Song <cadenzasong@me.com>
2018-01-20 14:26:43 -0800

    implementation

--- designs.js
+++ designs.js
@@ -4,7 +4,33 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
+	var numRows = $('#input_height').val();
+	var numColumns = $('#input_width').val();
+	console.log(numRows);
+	console.log(numColumns);
 
-// Your code goes here!
-
+	var table = $('#pixel_canvas');
+	table.attr('align', 'center');
+	$('#pixel_canvas_grid').remove();
+	var tableBody = $("<tbody id='pixel_canvas_grid'></tbody>");
+	var row = $('<tr></tr>');
+	for (var c = 0; c < numColumns; ++c) {
+		row.append($('<td></td>'));
+	}
+	for (var r = 0; r < numRows; ++r) {
+		tableBody.append(row.clone());
+	}
+	table.append(tableBody);
 }
+
+$(makeGrid);
+
+$('#sizePicker').on('submit', function(evt) {
+	evt.preventDefault(); // to stop the form from submitting
+	makeGrid();
+});
+
+$( '#pixel_canvas' ).on( 'click', 'td', function() {
+	var color = $('#colorPicker').val();
+	$(this).attr('bgcolor', color);
+});
